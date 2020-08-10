5cc4f083d1f6e302129c666f6701227243ac2685
Andrew Meyers <ameyers.central@gmail.com>
2020-08-10 15:59:20 -0400

    added styling + can remove todos from todos array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   todoArray = [];
-  addTodo(value) {
-    if (value !== "") {
-       this.todoArray.push(value)
-       console.log(this.todoArray) 
+  /* add item */
+  addItem(value) {
+    if (this.todoArray.includes(value)) {
+        alert('Todo already exists...');
+        return;
+    }
+    if (value !== '') {
+        this.todoArray.push(value);
+        console.log(this.todoArray);
     } else {
-      alert('No todo found...');
+        alert('No todo found...');
+    }
+  }
+  /* delete item */
+  deleteItem(todo) {
+    for (let i = 0; i < this.todoArray.length; i++) {
+      if (todo === this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+        console.log(this.todoArray);
+      }
     }
   }
 }
